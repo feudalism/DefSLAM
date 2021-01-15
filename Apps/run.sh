c6a2b925a925a048ef418e75ed79b2187d9756ab
Lea <feudalism@users.noreply.github.com>
2021-01-15 12:24:41 +0100

    vi.cc for mandala

--- Apps/run.sh
+++ Apps/run.sh
@@ -1,24 +1,33 @@
-
+### Args for DefSLAM ###
 orb_voc=/home/user3/slam/DefSLAM/Vocabulary/ORBvoc.txt
 
-## DATASETS
-# Hamlyn
-yaml=/home/user3/slam/datasets/defslam-f5phantom/hamlyn.yaml
-vid=/home/user3/slam/datasets/defslam-f5phantom/f5_dynamic_deint_L.avi
+## Datasets
+# # Hamlyn
+# yaml=/home/user3/slam/datasets/defslam-f5phantom/hamlyn.yaml
+# vid=/home/user3/slam/datasets/defslam-f5phantom/f5_dynamic_deint_L.avi
 
 # # Mandala
-# yaml=/home/user3/slam/datasets/mandala0/stereo0.yaml
-# vid=/home/user3/slam/datasets/mandala0/images
+yaml=/home/user3/slam/datasets/mandala0/stereo0_2.yaml
+imgs=/home/user3/slam/datasets/mandala0/images
+ts="/home/user3/slam/datasets/mandala0/timestamps/timestamps_copy.txt"
 
 
-## DefSLAM
+### Run DefSLAM ###
 # Process a sequence
 # Usage: ./DefSLAM ORBvocabulary calibrationFile video
-# or     ./DefSLAM ORBvocabulary calibrationFile
-
 echo Starting DefSLAM...
-echo '    Run one video without ground truth'
-echo ./DefSLAM $orb_voc $yaml $vid
-echo '    Running ...'
+echo $orb_voc $yaml $imgs $ts
 echo
-./DefSLAM $orb_voc $yaml $vid
+
+## Run type
+# # Debug
+gdb --args ./DefSLAM $orb_voc $yaml $imgs $ts
+# gdb --args ./DefSLAMGT $orb_voc $yaml $imgs $imgs $ts
+
+# # Normal run
+# ./DefSLAM $orb_voc $yaml $imgs $ts
+# ./DefSLAMGT $orb_voc $yaml $imgs $imgs $ts
+
+# # Valgrind
+# valgrind --leak-check=yes --log-file="valgrindoutput.txt" ./DefSLAM $orb_voc $yaml $vid $ts
+
--- Apps/vi.cc
+++ Apps/vi.cc
@@ -1,82 +1,59 @@
-#include<fstream>
-
 #include <opencv2/core/core.hpp>
 #include <System.h>
 
 void LoadMandalaImgs(const string &strImagePath, const string &strPathTimes,
-                vector<string> &vstrImages, vector<double> &vTimeStamps);
+                vector<string> &vstrImageFilenames, vector<double> &vTimeStamps);
 
 int main(int argc, char **argv)
 {
     // variable declarations for the arguments
     string orbVocab = argv[1];  
     string calibFile = argv[2];    
-    string videoFile = argv[3];
-    
-	// prints the number of arguments
-    std::cout << argc << std::endl;
+    string imgFolder = argv[3];   
+    string tsCamFile = argv[4];
     
 	// prints usage if wrong number of arguments
-    if ((argc != 4) and (argc != 3))
+    if (argc != 5)
     {
         cerr << endl
-             << "Usage: ./DefSLAM ORBvocabulary calibrationFile video" << endl
-             << " or    ./DefSLAM ORBvocabulary calibrationFile  " << endl
+             << "Usage: ./DefSLAM ORBvocabulary calibrationFile imgFolder timestampsFile" << endl
              << endl;
         return 1;
     }
+    
+    // initialise data containers, load data
+    vector<string> vstrImageFilenames;
+    vector<double> vTimeStampsCam;
 
-	// VideoCapture: class for video capturing (from video files, image seqs, cameras)
-	// initialise VideoCapture
-    cv::VideoCapture cap;
-	
-    if (argc == 3)
-    {
-		// open the default camera using the default API, for video capturing
-        std::cout << "Opening the default camera..." << std::endl;
-        cap.open(0);
-    }
-    else
-    {
-        // video file / capturing device / video stream
-        std::cout << "Opening video file: " << videoFile << std::endl;
-        cap.open(videoFile);
-    }
+    cout << "Loading images..." << endl;
+    LoadMandalaImgs(imgFolder, tsCamFile, vstrImageFilenames, vTimeStampsCam);
+    cout << "Loaded images!" << endl;
 
-    // check if we succeeded in initialising video capturing
-    if (!cap.isOpened()) 
-    {
-        std::cout << "Failed to open camera/video." << std::endl;
-        return -1;
-    }
-    else
-        std::cout << "Camera/video opened successfully." << std::endl;
-
-
+    const int nImages = vstrImageFilenames.size();
+    
     // Create SLAM system. It initializes all system threads (local mapping, loop closing, viewer)
     // and gets ready to process frames.
     // args: ORB vocab, calibration file, use viewer
     defSLAM::System SLAM(orbVocab, calibFile, true);
 
-    uint i(0);
-    cap.set(cv::CAP_PROP_FRAME_WIDTH, 640);
-    cap.set(cv::CAP_PROP_FRAME_HEIGHT, 480);
-
-    while (cap.isOpened())
+    cv::Mat im;
+    for(int ni=0; ni<nImages; ni++)
     {
-        // gets the capture as a matrix
-        cv::Mat imLeft;
-        cap >> imLeft;
-
-        if (imLeft.empty())
+        std::cout << vstrImageFilenames[ni] << " i:  " << ni << std::endl;
+        
+        // Read image from file
+        im = cv::imread(vstrImageFilenames[ni],cv::IMREAD_UNCHANGED);
+        if(im.empty())
         {
-            cerr << endl
-                 << "Failed to load image at: " << to_string(i) << endl;
+            cerr << endl << "Failed to load image at: "
+                 <<  vstrImageFilenames[ni] << endl;
             return 1;
         }
+        
+        double tframe = vTimeStampsCam[ni];
 
-        SLAM.TrackMonocular(imLeft, i);
-        i++;
+        // Pass the image to the SLAM system
+        SLAM.TrackMonocular(im,ni);
     }
 
     SLAM.Shutdown();
@@ -85,12 +62,12 @@ int main(int argc, char **argv)
 }
 
 void LoadMandalaImgs(const string &strImagePath, const string &strPathTimes,
-                vector<string> &vstrImages, vector<double> &vTimeStamps)
+                vector<string> &vstrImageFilenames, vector<double> &vTimeStamps)
 {
     ifstream fTimes;
     fTimes.open(strPathTimes.c_str());
     vTimeStamps.reserve(5000);
-    vstrImages.reserve(5000);
+    vstrImageFilenames.reserve(5000);
     
     while(!fTimes.eof())
     {
@@ -99,15 +76,17 @@ void LoadMandalaImgs(const string &strImagePath, const string &strPathTimes,
         
         if(!s.empty())
         {
-            // image
             stringstream ss;
             ss << s;
-            vstrImages.push_back(strImagePath + "/stereo_im_l_" + ss.str() + ".png");
             
-            // timestamp
             double t;
             ss >> t;
-            vTimeStamps.push_back(t);
+            vTimeStamps.push_back(t * 1e-6);
+            
+            // image
+            string filename = ss.str();
+            filename.erase(filename.length() - 6);            
+            vstrImageFilenames.push_back(strImagePath + "/stereo_im_l_" + filename + ".png");
 
         }
     }
