8136146ae0332a848950c6ea5e52846cf786fc2e
Lea <feudalism@users.noreply.github.com>
2021-02-04 17:42:20 +0100

    mv ImuTypes to defSLAM library/namespace

--- Thirdparty/ORBSLAM_3/include/G2oTypes.h
+++ Thirdparty/ORBSLAM_3/include/G2oTypes.h
@@ -499,7 +499,7 @@ class EdgeInertial : public g2o::BaseMultiEdge<9,Vector9d>
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    EdgeInertial(IMU::Preintegrated* pInt);
+    EdgeInertial(defSLAM::IMU::Preintegrated* pInt);
 
     virtual bool read(std::istream& is){return false;}
     virtual bool write(std::ostream& os) const{return false;}
@@ -540,7 +540,7 @@ public:
 
     const Eigen::Matrix3d JRg, JVg, JPg;
     const Eigen::Matrix3d JVa, JPa;
-    IMU::Preintegrated* mpInt;
+    defSLAM::IMU::Preintegrated* mpInt;
     const double dt;
     Eigen::Vector3d g;
 };
@@ -552,8 +552,8 @@ class EdgeInertialGS : public g2o::BaseMultiEdge<9,Vector9d>
 public:
     EIGEN_MAKE_ALIGNED_OPERATOR_NEW
 
-    // EdgeInertialGS(IMU::Preintegrated* pInt);
-    EdgeInertialGS(IMU::Preintegrated* pInt);
+    // EdgeInertialGS(defSLAM::IMU::Preintegrated* pInt);
+    EdgeInertialGS(defSLAM::IMU::Preintegrated* pInt);
 
     virtual bool read(std::istream& is){return false;}
     virtual bool write(std::ostream& os) const{return false;}
@@ -563,7 +563,7 @@ public:
 
     const Eigen::Matrix3d JRg, JVg, JPg;
     const Eigen::Matrix3d JVa, JPa;
-    IMU::Preintegrated* mpInt;
+    defSLAM::IMU::Preintegrated* mpInt;
     const double dt;
     Eigen::Vector3d g, gI;
 
@@ -723,7 +723,7 @@ public:
         // H = es.eigenvectors()*eigs.asDiagonal()*es.eigenvectors().transpose();
     // }
     // ConstraintPoseImu(const cv::Mat &Rwb_, const cv::Mat &twb_, const cv::Mat &vwb_,
-                       // const IMU::Bias &b, const cv::Mat &H_)
+                       // const defSLAM::IMU::Bias &b, const cv::Mat &H_)
     // {
         // Rwb = Converter::toMatrix3d(Rwb_);
         // twb = Converter::toVector3d(twb_);
