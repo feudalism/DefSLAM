daad2b4cbeb0fdd683e90d3590dc1ea3beab53fd
Lea <salehah.ar@gmail.com>
2021-02-16 14:42:53 +0100

    modified LocalMapping::Run() to be similar to OS3, using mpAtlas

--- Thirdparty/ORBSLAM_2/src/LocalMapping.cc
+++ Thirdparty/ORBSLAM_2/src/LocalMapping.cc
@@ -62,7 +62,7 @@ namespace ORB_SLAM2
       SetAcceptKeyFrames(false);
 
       // Check if there are keyframes in the queue
-      if (CheckNewKeyFrames())
+      if (CheckNewKeyFrames() && !mbBadImu)
       {
         // BoW conversion and insertion in Map
         ProcessNewKeyFrame();
@@ -73,29 +73,117 @@ namespace ORB_SLAM2
 
         // Triangulate new MapPoints
         CreateNewMapPoints();
+        
+        mbAbortBA = false;
 
         if (!CheckNewKeyFrames())
         {
           // Find more matches in neighbor keyframes and fuse point duplications
           SearchInNeighbors();
         }
-
-        mbAbortBA = false;
+        
+        int num_FixedKF_BA = 0;
 
         if (!CheckNewKeyFrames() && !stopRequested())
         {
           // Local BA
-          if (mpMap->KeyFramesInMap() > 2)
-            Optimizer::LocalBundleAdjustment(mpCurrentKeyFrame, &mbAbortBA,
-                                             mpMap);
+            if (mpAtlas->KeyFramesInMap() > 2)
+            {
+                if(mbInertial && mpCurrentKeyFrame->GetMap()->isImuInitialized())
+                {
+                    float dist = cv::norm(mpCurrentKeyFrame->mPrevKF->GetCameraCenter() - mpCurrentKeyFrame->GetCameraCenter()) +
+                            cv::norm(mpCurrentKeyFrame->mPrevKF->mPrevKF->GetCameraCenter() - mpCurrentKeyFrame->mPrevKF->GetCameraCenter());
 
-          // Check redundant local Keyframes
-          KeyFrameCulling();
+                    if(dist>0.05)
+                        mTinit += mpCurrentKeyFrame->mTimeStamp - mpCurrentKeyFrame->mPrevKF->mTimeStamp;
+                    if(!mpCurrentKeyFrame->GetMap()->GetInertialBA2())
+                    {
+                        if((mTinit<10.f) && (dist<0.02))
+                        {
+                            cout << "Not enough motion for initializing. Reseting..." << endl;
+                            unique_lock<mutex> lock(mMutexReset);
+                            mbResetRequestedActiveMap = true;
+                            mpMapToReset = mpCurrentKeyFrame->GetMap();
+                            mbBadImu = true;
+                        }
+                    }
+
+                    bool bLarge = ((mpTracker->GetMatchesInliers()>75)&&mbMonocular)||((mpTracker->GetMatchesInliers()>100)&&!mbMonocular);
+                    Optimizer::LocalInertialBA(mpCurrentKeyFrame, &mbAbortBA, mpCurrentKeyFrame->GetMap(), bLarge, !mpCurrentKeyFrame->GetMap()->GetInertialBA2());
+                }
+                else
+                {
+                    std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
+                    Optimizer::LocalBundleAdjustment(mpCurrentKeyFrame,&mbAbortBA, mpCurrentKeyFrame->GetMap(),num_FixedKF_BA);
+                    std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
+                }
+            }
+
+          // Initialize IMU here
+            if(!mpCurrentKeyFrame->GetMap()->isImuInitialized() && mbInertial)
+            {
+                if (mbMonocular)
+                    InitializeIMU(1e2, 1e10, true);
+                else
+                    InitializeIMU(1e2, 1e5, true);
+            }
+
+            // Check redundant local Keyframes
+            KeyFrameCulling();
+            
+            if ((mTinit<100.0f) && mbInertial)
+            {
+                if(mpCurrentKeyFrame->GetMap()->isImuInitialized() && mpTracker->mState==Tracking::OK) // Enter here everytime local-mapping is called
+                {
+                    if(!mpCurrentKeyFrame->GetMap()->GetInertialBA1()){
+                        if (mTinit>5.0f)
+                        {
+                            cout << "start VIBA 1" << endl;
+                            mpCurrentKeyFrame->GetMap()->SetInertialBA1();
+                            if (mbMonocular)
+                                InitializeIMU(1.f, 1e5, true); // 1.f, 1e5
+                            else
+                                InitializeIMU(1.f, 1e5, true); // 1.f, 1e5
+
+                            cout << "end VIBA 1" << endl;
+                        }
+                    }
+                    //else if (mbNotBA2){
+                    else if(!mpCurrentKeyFrame->GetMap()->GetInertialBA2()){
+                        if (mTinit>15.0f){ // 15.0f
+                            cout << "start VIBA 2" << endl;
+                            mpCurrentKeyFrame->GetMap()->SetInertialBA2();
+                            if (mbMonocular)
+                                InitializeIMU(0.f, 0.f, true); // 0.f, 0.f
+                            else
+                                InitializeIMU(0.f, 0.f, true);
+
+                            cout << "end VIBA 2" << endl;
+                        }
+                    }
+
+                    // scale refinement
+                    if (((mpAtlas->KeyFramesInMap())<=100) &&
+                            ((mTinit>25.0f && mTinit<25.5f)||
+                            (mTinit>35.0f && mTinit<35.5f)||
+                            (mTinit>45.0f && mTinit<45.5f)||
+                            (mTinit>55.0f && mTinit<55.5f)||
+                            (mTinit>65.0f && mTinit<65.5f)||
+                            (mTinit>75.0f && mTinit<75.5f))){
+                        cout << "start scale ref" << endl;
+                        if (mbMonocular)
+                            ScaleRefinement();
+                        cout << "end scale ref" << endl;
+                    }
+                }
+            }
+        
         }
+        
         if (mpLoopCloser)
           mpLoopCloser->InsertKeyFrame(mpCurrentKeyFrame);
       }
-      else if (Stop())
+      else if (Stop() && !mbBadImu)
       {
         // Safe area to stop
         while (isStopped() && !CheckFinish())
@@ -107,6 +195,9 @@ namespace ORB_SLAM2
       }
 
       ResetIfRequested();
+
+      // Tracking will see that Local Mapping is busy
+      SetAcceptKeyFrames(true);
 
       // Tracking will see that Local Mapping is busy
 
@@ -767,7 +858,7 @@ namespace ORB_SLAM2
                         pKF->mPrevKF = NULL;
                         pKF->setBadFlag();
                     }
-                    else if(!mpCurrentKeyFrame->GetMap()->GetIniertialBA2() && (cv::norm(pKF->GetImuPosition()-pKF->mPrevKF->GetImuPosition())<0.02) && (t<3))
+                    else if(!mpCurrentKeyFrame->GetMap()->GetInertialBA2() && (cv::norm(pKF->GetImuPosition()-pKF->mPrevKF->GetImuPosition())<0.02) && (t<3))
                     {
                         pKF->mNextKF->mpImuPreintegrated->MergePrevious(pKF->mpImuPreintegrated);
                         pKF->mNextKF->mPrevKF = pKF->mPrevKF;
