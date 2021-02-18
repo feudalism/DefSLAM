fc6ac5cafc746964f354696ab46343edd4ff44a5
Lea <salehah.ar@gmail.com>
2021-02-18 09:33:10 +0100

    executables

--- Apps/debug.sh
+++ Apps/debug.sh
@@ -1,1 +1,11 @@
+# Frame ctor - check NLeft value
+b Frame.cc:401
+
+# Tracking::GrabImageMonocular - after Frame creation
+b Tracking.cc:228
+
+# Tracking::Track() - start of fcn
+b Tracking.cc:448
+
+info breakpoints
 run
--- Apps/recompile.sh
+++ Apps/recompile.sh
@@ -1,5 +1,5 @@
 echo Recompiling DefSLAM...
 cd ../build
 cmake .. -DCMAKE_BUILD_TYPE=Debug
-make -j 4
+make -j 3
 cd ../Apps
