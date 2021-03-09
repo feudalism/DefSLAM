21f1d506218a42281ca4b887d6db97ddbc083191
sal <salehahr@users.noreply.github.com>
2021-03-09 13:55:59 +0100

    [wip] implementing opencv's Kalman filter

--- Apps/vi.cc
+++ Apps/vi.cc
@@ -6,6 +6,9 @@
 #include <string.h> 
 #include <unistd.h> 
 
+#include <opencv2/video/video.hpp>
+#include <random>
+
 void LoadMandalaImgs(const string &strImagePath, const string &strPathTimes,
                 vector<string> &vstrImageFilenames, vector<double> &vTimeStamps);
 
@@ -31,7 +34,7 @@ int main(int argc, char **argv)
     }
                 
     // initialise data containers
-    string strGTTrajPath = "/home/user3/slam/DefSLAM/Apps/traj_mandala0_gt_noisy.txt";
+    string strGTTrajPath = "traj_mandala0_gt_noisy.txt";
     vector<string> vstrImageFilenames;
     vector<double> vTimeStampsCam;
     vector<double> vTimeStampsGT;
@@ -48,6 +51,33 @@ int main(int argc, char **argv)
 
     const int nImages = vstrImageFilenames.size();
     
+    // Kalman Filter
+    int stateSize = 3;
+    int measSize = 3;
+    int contrSize = 3;
+    cv::KalmanFilter kf(stateSize, measSize, contrSize, CV_32F);
+
+    cv::Mat state(stateSize, 1, CV_32F);  // [x,y,z]
+    cv::Mat meas(measSize, 1, CV_32F);    // [zx, zy, zz]
+    cv::Mat control(contrSize, 1, CV_32F);    // [ux, uy, uz]
+
+    cv::setIdentity(kf.transitionMatrix);
+    cv::setIdentity(kf.controlMatrix);
+    cv::setIdentity(kf.measurementMatrix);
+
+    const double mean = 0.0;
+    const double stddev = 0.05;
+    std::default_random_engine generator;
+    std::normal_distribution<double> distribution(mean, stddev);
+
+    // kf.processNoiseCov.at<float>(0) = distribution(generator);
+    // kf.processNoiseCov.at<float>(4) = distribution(generator);
+    // kf.processNoiseCov.at<float>(8) = distribution(generator);
+    // std::cout << kf.processNoiseCov << std::endl;
+
+    setIdentity(kf.processNoiseCov, cv::Scalar(0.1));
+    setIdentity(kf.measurementNoiseCov, cv::Scalar(0.05));
+
     // Create SLAM system. It initializes all system threads (local mapping, loop closing, viewer)
     // and gets ready to process frames.
     // args: ORB vocab, calibration file, use viewer
@@ -57,7 +87,6 @@ int main(int argc, char **argv)
     ofstream f;
     f.open("./trajectory.txt");
     f << fixed;
-    cout << endl << "Saving camera trajectory to trajectory.txt" << endl;
 
     cv::Mat im;
     size_t start = 200;
@@ -65,7 +94,7 @@ int main(int argc, char **argv)
     for(int ni=start; ni<nImages; ni++)
     {
         std::cout << vstrImageFilenames[ni] << " i:  " << ni << std::endl;
-        
+
         // Read image from file
         im = cv::imread(vstrImageFilenames[ni],cv::IMREAD_UNCHANGED);
         if(im.empty())
@@ -74,26 +103,50 @@ int main(int argc, char **argv)
                  <<  vstrImageFilenames[ni] << endl;
             return 1;
         }
-        
+
         double tframe = vTimeStampsCam[ni];
 
         // Pass the image to the SLAM system
         SLAM.TrackMonocular(im,ni);
         
         // Force update trajectory
-        if((ni >= 230) && (ni < 240))
+        if((ni >= 220) && (ni < 240))
         {
             std::cout << "------------- --------------" << std::endl;
+        
+            // Initialise Kalman filter
+            if(ni == 220)
+            {
+                std::cout << "Kalman initial state (at frame " << ni << ") : " << std::endl;
+                kf.statePre = SLAM.getCoordinates();
+                std::cout << kf.statePre << std::endl;
+            }
+
+            // predict
+            control.at<float>(0) = distribution(generator);
+            control.at<float>(1) = distribution(generator);
+            control.at<float>(2) = distribution(generator);
+            state = kf.predict(control);
+            std::cout << "ni: " << ni << " predicted" << std::endl;
+            std::cout << state << std::endl;
+
+            // measure
             int idx = ni - start;
-            double x = vX[idx];
-            double y = vY[idx];
-            double z = vZ[idx];
+            meas.at<float>(0) = vX[idx];
+            meas.at<float>(1) = vY[idx];
+            meas.at<float>(2) = vZ[idx];
+            kf.correct(meas);
+            std::cout << "ni: " << ni << " measured" << std::endl;
+            std::cout << meas << std::endl;
+
             double qx = vqx[idx];
             double qy = vqy[idx];
             double qz = vqz[idx];
             double qw = vqw[idx];
 
-            SLAM.updateTrajectory(x, y, z, qx, qy, qz, qw);
+            SLAM.updateTrajectory(kf.statePost.at<float>(0), kf.statePost.at<float>(1),
+                    kf.statePost.at<float>(2),
+                    qx, qy, qz, qw);
         }
         
         // Save trajectory to text file
