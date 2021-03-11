dd566b86b5558ec03dde0e0cad0283d63608f482
sal <salehahr@users.noreply.github.com>
2021-03-11 18:08:09 +0100

    plots for offline kalman

--- Apps/kalman_offline.sh
+++ Apps/kalman_offline.sh
@@ -4,6 +4,12 @@ if g++ kalman_offline.cpp -o kalman_offline -I /usr/local/include/opencv4 -lopen
     echo "Running"
     ./kalman_offline
     # gdb ./kalman_offline
+    
+    # Plot
+    echo "Plotting results"
+    cd ..
+    python3 plot_kalman.py
+    cd Apps/
 else
     echo "Run failed."
 fi
---
+++ plot_kalman.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+from traj_parser import parse, plot
+
+traj_data = {
+            'stereoGT': "./Apps/traj_mandala0_gt.txt",
+            # 'measurements': "./Apps/traj_mandala0_gt_imu_noisy.txt",
+            'rw': "./Apps/trajectory_offline_rw.txt",
+            'kalman' : "./Apps/trajectory_offline.txt",
+            'mono' : "./Apps/traj_mandala0_mono.txt",
+            }
+data_labels = ['ts', 'x', 'y', 'z', 'q1', 'q2', 'q3', 'q4']
+    
+axes = None
+for label, filepath in traj_data.items():
+    data_containers = parse(filepath, data_labels)
+    t_arr = data_containers['ts']
+    
+    if label == 'stereoGT' or label == 'measurements':
+        min_t = min(t_arr)
+        max_t = max(t_arr)
+    elif label != 'rw':
+        min_t = min([min_t, min(t_arr)])
+        max_t = min([max_t, max(t_arr)])
+    
+    axes = plot(t_arr, data_containers, data_labels, label, min_t, max_t, axes=axes)
+    
+plt.legend()
+plt.show()
