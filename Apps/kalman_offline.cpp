dd8b188634f48732f1e284b1e5158dd20e30a054
sal <salehahr@users.noreply.github.com>
2021-03-11 18:14:41 +0100

    incorporate noisy imu in kalman_offline using vImuQueue

--- Apps/kalman_offline.cpp
+++ Apps/kalman_offline.cpp
@@ -8,15 +8,21 @@
 #include <random>
 
 void loadTrajectory(const std::string &strGTTrajPath,
-                std::vector<float> &vTimeStampsGT,
+                std::vector<float> &vTimeStampsIMU,
                 std::vector<float> &vX, std::vector<float> &vY, std::vector<float> &vZ,
                 std::vector<float> &vq1, std::vector<float> &vq2, std::vector<float> &vq3, std::vector<float> &vq4);
 
 void saveTrajectory(std::ofstream &f, const float &t, const float &x, const float &y, const float &z,
         const float &q1, const float &q2, const float &q3, const float &q4);
 
+size_t loadImuQueue(std::vector<float> &vImuQueue, 
+        size_t firstImuIndex,
+        std::vector<float> &vTimeStampsImu,
+        const float &currentFrameTs, std::vector<float> &vMeasurement);
+
 cv::KalmanFilter createKF();
-void initialiseKF(cv::KalmanFilter &kf, const cv::Mat state);
+void initialiseKF(cv::KalmanFilter &kf);
+void setPostValsToOptimVals(cv::KalmanFilter &kf, const cv::Mat &state);
 
 std::ofstream openFile(const std::string &filepath);
 
@@ -29,11 +35,11 @@ int main(int argc, char **argv)
     loadTrajectory(strMonoTrajPath, vTimeStampsMono,
             vxmono,  vymono, vzmono, vqxmono, vqymono, vqzmono, vqwmono);
 
-    // load noisy GT data
-    std::string strGTTrajPath = "traj_mandala0_gt_noisy.txt";
-    std::vector<float> vTimeStampsGT;
+    // load noisy imu data
+    std::string strGTTrajPath = "traj_mandala0_gt_imu_noisy.txt";
+    std::vector<float> vTimeStampsIMU;
     std::vector<float> vxmeas, vymeas, vzmeas, vqxmeas, vqymeas, vqzmeas, vqwmeas;
-    loadTrajectory(strGTTrajPath, vTimeStampsGT,
+    loadTrajectory(strGTTrajPath, vTimeStampsIMU,
             vxmeas,  vymeas, vzmeas, vqxmeas, vqymeas, vqzmeas, vqwmeas);
 
     // kalman filter
@@ -50,7 +56,7 @@ int main(int argc, char **argv)
     std::ofstream f_rw = openFile("./trajectory_offline_rw.txt");
 
     // loop
-    const int nImages = vTimeStampsGT.size();
+    const int nImages = vTimeStampsMono.size();
     size_t start = 200;
     size_t kalman_start = 220;
     size_t kalman_end = 250;
@@ -67,45 +73,71 @@ int main(int argc, char **argv)
     cv::Mat meas(measSize, 1, CV_32F);
     cv::Mat rw(stateSize, 1, CV_32F);
 
-    for(int ni=start; ni<(nImages + start); ni++)
-    {
-        int idx = ni - start;
+    std::vector<float> vImuXQueue, vImuYQueue, vImuZQueue;
+    // find index of first IMU data that appears after or during the first frame
+    int firstImuIndex = 0;
+    while(vTimeStampsIMU[firstImuIndex] <= vTimeStampsMono[0])
+      firstImuIndex++;
+    firstImuIndex--;
 
-        // monocular coordinates
+    for(size_t ni=start; ni<(nImages + start); ni++)
+    {
+        std::cout << "ni " << ni << " -- ";
+        const int idx = ni - start;
+        const float currentFrameTs = vTimeStampsMono[idx];
+
+        // monocular coordinates from optimisation
         state.at<float>(0) = vxmono[idx];
         state.at<float>(1) = vymono[idx];
         state.at<float>(2) = vzmono[idx];
 
-        // monocular quaternions
+        // monocular quaternions from optimisation
         float qx = vqxmono[idx];
         float qy = vqymono[idx];
         float qz = vqzmono[idx];
         float qw = vqwmono[idx];
 
-        kf.statePost = state;
-
         if(ni == (kalman_start - 1))
-            initialiseKF(kf, state.clone());
+            initialiseKF(kf);
+
+        setPostValsToOptimVals(kf, state);
+
+        // load IMU measurements to next frame
+        if(idx > 0)
+        {
+            loadImuQueue(vImuXQueue, firstImuIndex,
+                vTimeStampsIMU, currentFrameTs, vxmeas);
+            loadImuQueue(vImuYQueue, firstImuIndex,
+                vTimeStampsIMU, currentFrameTs, vymeas);
+            firstImuIndex = loadImuQueue(vImuZQueue, firstImuIndex,
+                vTimeStampsIMU, currentFrameTs, vzmeas);
+        }
 
         // kalman implementation
-        if((ni >= kalman_start))// && (ni < kalman_end))
+        if((ni >= kalman_start) && (ni < kalman_end))
         {
-            // predict
-            // random walk          
-            cv::randn(control, mean, stddev);
+            // std::vector<float> vImuXQueue(&vxmeas[idx], &vxmeas[idx+1]);
+            std::for_each(std::begin(vImuXQueue), std::end(vImuXQueue), [](float const& value) {
+                std::cout << value << " ";
+            });
+            std::cout << std::endl;
 
-            // set coordinates from DefSLAM to be 
-            rw = kf.predict(control);
-            saveTrajectory(f_rw, ni,
-                rw.at<float>(0), rw.at<float>(1), rw.at<float>(2),
-                qx, qy, qz, qw);
+            // // predict
+            // // random walk          
+            // cv::randn(control, mean, stddev);
 
-            // correct
-            // noisy stereo gt coordinates
-            meas.at<float>(0) = vxmeas[idx];
-            meas.at<float>(1) = vymeas[idx];
-            meas.at<float>(2) = vzmeas[idx];
-            kf.correct(meas);
+            // // set coordinates from DefSLAM to be 
+            // rw = kf.predict(control);
+            // saveTrajectory(f_rw, ni,
+                // rw.at<float>(0), rw.at<float>(1), rw.at<float>(2),
+                // qx, qy, qz, qw);
+
+            // // correct
+            // // noisy stereo gt coordinates
+            // meas.at<float>(0) = vxmeas[idx];
+            // meas.at<float>(1) = vymeas[idx];
+            // meas.at<float>(2) = vzmeas[idx];
+            // kf.correct(meas);
 
         }
 
@@ -117,7 +149,7 @@ int main(int argc, char **argv)
     f_rw.close();
 }
 
-void loadTrajectory(const std::string &strGTTrajPath, std::vector<float> &vTimeStampsGT,
+void loadTrajectory(const std::string &strGTTrajPath, std::vector<float> &vTimeStampsIMU,
                 std::vector<float> &vxmeas, std::vector<float> &vymeas, std::vector<float> &vzmeas,
                 std::vector<float> &vqxmeas, std::vector<float> &vqymeas, std::vector<float> &vqzmeas, std::vector<float> &vqwmeas)
 {
@@ -144,7 +176,7 @@ void loadTrajectory(const std::string &strGTTrajPath, std::vector<float> &vTimeS
             item = s.substr(0, pos);
             data[7] = stod(item);
 
-            vTimeStampsGT.push_back(data[0]);
+            vTimeStampsIMU.push_back(data[0]);
             vxmeas.push_back(data[1]);
             vymeas.push_back(data[2]);
             vzmeas.push_back(data[3]);
@@ -188,13 +220,18 @@ cv::KalmanFilter createKF()
     return kf;
 }
 
-void initialiseKF(cv::KalmanFilter &kf, const cv::Mat state)
+void initialiseKF(cv::KalmanFilter &kf)
 {
     setIdentity(kf.processNoiseCov, cv::Scalar(0.05));
     setIdentity(kf.measurementNoiseCov, cv::Scalar(0.05));
+    setIdentity(kf.errorCovPost, cv::Scalar(1));
+}
 
-    kf.statePost = state; // same pointers
-    setIdentity(kf.errorCovPre, cv::Scalar(1));
+void setPostValsToOptimVals(cv::KalmanFilter &kf, const cv::Mat &state)
+{
+    kf.statePost = state;
+    kf.errorCovPost = kf.transitionMatrix * kf.errorCovPost.t() * kf.transitionMatrix
+            + kf.processNoiseCov;
 }
 
 std::ofstream openFile(const std::string &filepath)
@@ -203,4 +240,25 @@ std::ofstream openFile(const std::string &filepath)
     f.open(filepath.c_str());
     f << std::fixed;
     return f;
+}
+
+size_t loadImuQueue(std::vector<float> &vImuQueue, 
+        size_t firstImuIndex,
+        std::vector<float> &vTimeStampsImu,
+        const float &currentFrameTs, std::vector<float> &vMeasurement)
+{
+    vImuQueue.clear();
+    while(vTimeStampsImu[firstImuIndex] <= currentFrameTs)
+    {
+        vImuQueue.push_back(vMeasurement[firstImuIndex]);
+
+        // prevent out of bound indexing when reaching end of IMU input
+        if(firstImuIndex < vTimeStampsImu.size())
+            firstImuIndex++;
+        else
+            return firstImuIndex;
+    }
+
+    return firstImuIndex;
+
 }
